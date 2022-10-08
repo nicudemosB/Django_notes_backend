e969eaf3ea81559f4b3a9503065a858f4c67a066
Billy <billyn0213@gmail.com>
2022-10-08 08:33:03 -0400

    using .env

--- mynotes/mynotes/settings.py
+++ mynotes/mynotes/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sdwz-fqq$5$(2-we%%urh(ve6*%4=qrmh1)cr#b*tz^8nlmniu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
